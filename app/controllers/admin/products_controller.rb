8dbf69f0d7a44aef14b739b7413ad5136279c173
Eddie Kao <eddie@digik.com.tw>
2016-05-22 14:54:17 +0800

    add "add product" feature

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,6 +3,7 @@ class Admin::ProductsController < ApplicationController
   layout "admin"
 
   def index
+    @products = Product.all
   end
 
   def new
@@ -10,5 +11,16 @@ class Admin::ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: "新增商品成功"
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
   end
 end
